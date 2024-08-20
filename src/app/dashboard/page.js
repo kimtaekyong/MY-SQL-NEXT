bcbf97095f3cc25b4d332270f3978790e552e640
kimtaekyong <lkss98@gmail.com>
2024-08-20 18:01:22 +0900

    dashborad 추가

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+// 서버 컴포넌트에서 데이터 가져오기
+export default async function Dashboard() {
+  // 데이터 렌더링
+  return (
+    <div className="h-[100vh] flex justify-center items-center flex-col">
+      <h2 className="text-2xl">로그인 완료되셨습니다.</h2>
+    </div>
+  );
+}
